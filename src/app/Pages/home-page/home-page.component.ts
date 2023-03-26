7005476b4a028035c42883ff691c0c65b10fb83d
tyhow99 <tmh00011@mix.wvu.edu>
2023-03-26 18:19:18 -0400

    Added AHL side bar

--- src/app/Pages/home-page/home-page.component.ts
+++ src/app/Pages/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import { GridModel } from 'src/app/grid1/grid1Model';
 import { transferlist } from 'src/app/grid1/Transferlist';
 import { NHL_List } from 'src/app/team-side-bar/NHLTeamList';
 import { TeamSideBar } from 'src/app/team-side-bar/teamSideBarModel';
+import { AHL_List } from 'src/app/team-side-bar/AHLTeamList';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -14,6 +15,7 @@ export class HomePageComponent {
   sidebar: SideBar [] = [];
   transferGrid: GridModel [] = [];
   tSideBar: TeamSideBar [] = [];
+  tSideBar2: TeamSideBar [] = [];
   constructor(){
     for(var item of mock_list){
       console.log(item);
@@ -28,6 +30,11 @@ export class HomePageComponent {
     {
       console.log(item3);
       this.tSideBar.push(item3);
+    }
+    for(var item4 of AHL_List)
+    {
+      console.log(item4);
+      this.tSideBar2.push(item4);
     }
   }
 }
---
+++ src/app/team-side-bar/AHLTeamList.ts
@@ -0,0 +1,70 @@
+export const AHL_List = [
+    {
+        team1: "Abbotsford Canucks",
+        team2: "Lehigh Valley Phantoms"
+    },
+    {
+        team1: "Bakersfield Condors",
+        team2: "Manitoba Moose"
+    },
+    {
+        team1: "Belleville Senators",
+        team2: "Milwaukee Admirals"
+    },
+    {
+        team1: "Bridgeport Islanders",
+        team2: "Ontario Reign"
+    },
+    {
+        team1: "Calgary Flames",
+        team2: "Ottawa Senators"
+    },
+    {
+        team1: "Calgary Wranglers",
+        team2: "Providence Bruins"
+    },
+    {
+        team1: "Charlotte Checkers",
+        team2: "Rochester Americans"
+    },
+    {
+        team1: "Chicago Wolves",
+        team2: "Rockford IceHogs"
+    },
+    {
+        team1: "Cleveland Monsters",
+        team2: "San Diego Gulls"
+    },
+    {
+        team1: "Coachella Valley Firebirds ",
+        team2: "San Jose Barracuda"
+    },
+    {
+        team1: "Colorado Eagles",
+        team2: "Springfield Thunderbirds"
+    },
+    {
+        team1: "Grand Rapids Griffins ",
+        team2: "Syracuse Crunch"
+    },
+    {
+        team1: "Hartford Wolf Pack",
+        team2: "Texas Stars"
+    },
+    {
+        team1: "Henderson Silver Knights ",
+        team2: "Toronto Marlies"
+    },
+    {
+        team1: "Hershey Bears",
+        team2: "Tucson Roadrunners"
+    },
+    {
+        team1: "Iowa Wild",
+        team2: "Utica Comets"
+    },
+    {
+        team1: "Laval Rocket",
+        team2: "Wilkes-Barre/Scranton Penguins"
+    },
+]
