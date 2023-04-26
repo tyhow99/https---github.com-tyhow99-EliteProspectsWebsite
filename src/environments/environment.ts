31f6cafc3f4fc5e5f4927e4b6c70c2c02a159844
tyhow99 <tmh00011@mix.wvu.edu>
2023-04-26 15:27:26 -0400

    Readded firebse URL deploys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,36 +1,36 @@
 export const environment = {
-  // firebase: {
-  //   projectId: 'eliteprospects-89f23',
-  //   appId: '1:637574595341:web:17f355f7f11dab9e0be644',
-  //   databaseURL: 'https://eliteprospects-89f23-default-rtdb.firebaseio.com',
-  //   storageBucket: 'eliteprospects-89f23.appspot.com',
-  //   apiKey: 'AIzaSyD_d0ZSjxP83eqP4R3bQLe82ZtVcAhw9aY',
-  //   authDomain: 'eliteprospects-89f23.firebaseapp.com',
-  //   messagingSenderId: '637574595341',
-  //   measurementId: 'G-JJRN4YF7N7',
-  // },
+  firebase: {
+    projectId: 'eliteprospects-89f23',
+    appId: '1:637574595341:web:17f355f7f11dab9e0be644',
+    databaseURL: 'https://eliteprospects-89f23-default-rtdb.firebaseio.com',
+    storageBucket: 'eliteprospects-89f23.appspot.com',
+    apiKey: 'AIzaSyD_d0ZSjxP83eqP4R3bQLe82ZtVcAhw9aY',
+    authDomain: 'eliteprospects-89f23.firebaseapp.com',
+    messagingSenderId: '637574595341',
+    measurementId: 'G-JJRN4YF7N7',
+  },
     production: false
 }
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD_d0ZSjxP83eqP4R3bQLe82ZtVcAhw9aY",
-  authDomain: "eliteprospects-89f23.firebaseapp.com",
-  databaseURL: "https://eliteprospects-89f23-default-rtdb.firebaseio.com",
-  projectId: "eliteprospects-89f23",
-  storageBucket: "eliteprospects-89f23.appspot.com",
-  messagingSenderId: "637574595341",
-  appId: "1:637574595341:web:463d595dabd5a4bf0be644",
-  measurementId: "G-B5XYJFK6CY"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD_d0ZSjxP83eqP4R3bQLe82ZtVcAhw9aY",
+//   authDomain: "eliteprospects-89f23.firebaseapp.com",
+//   databaseURL: "https://eliteprospects-89f23-default-rtdb.firebaseio.com",
+//   projectId: "eliteprospects-89f23",
+//   storageBucket: "eliteprospects-89f23.appspot.com",
+//   messagingSenderId: "637574595341",
+//   appId: "1:637574595341:web:463d595dabd5a4bf0be644",
+//   measurementId: "G-B5XYJFK6CY"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
