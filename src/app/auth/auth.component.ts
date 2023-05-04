9673b1b36d323c505092aaef7fb5218462e767d7
tyhow99 <tmh00011@mix.wvu.edu>
2023-05-04 10:30:04 -0400

    added authentification

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { AuthService } from "./auth.service";
 import { NgForm } from "@angular/forms";
+import { AuthResponse } from "./authResponse.model";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'auth',
@@ -8,14 +10,31 @@ import { NgForm } from "@angular/forms";
 })
 
 export class AuthComponent {
+    public buttonClicked!: string;
+    private authObservable!: Observable<AuthResponse>;
     constructor(private authService: AuthService){
 
     }
     public onSubmit(data: NgForm){
-        console.log("Button Clicked")
+        console.log("Button Clicked " + this.buttonClicked)
         console.log(data)
-        this.authService.signup(data.value.email,data.value.password).subscribe(data =>{
-            //console.log(data);
-        });
+
+        if(this.buttonClicked == 'SignUp'){
+            this.authObservable = this.authService.signup(data.value.email,data.value.password);
+        }
+        if(this.buttonClicked == 'Login'){
+            this.authObservable = this.authService.signup(data.value.email,data.value.password);
+        }
+    
+       
+       this.authObservable.subscribe(
+            (data:AuthResponse) =>{
+            console.log(data);
+        },
+       error => {
+            console.log(error.error);
+        }
+        );
+        data.resetForm();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { AuthResponse } from "./authResponse.model";
 
 @Injectable({
     providedIn: 'root'
@@ -8,6 +9,7 @@ import { environment } from "src/environments/environment";
 export class AuthService{
     baseUrl: string = "https://identitytoolkit.googleapis.com/vl/accounts" 
     signUpEndPoint: string = "signUp";
+    signInEndPoint: string = "signInWithPassword"
     private constructor(private http: HttpClient){
 
     }
@@ -17,6 +19,17 @@ export class AuthService{
             "password":password,
             "returnSecureToken": true,
         }
-        return this.http.post(`${this.baseUrl}:${this.signUpEndPoint}?keys=${environment.firebase.apiKey}`,requestBody)
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signUpEndPoint + '?' + 
+        'keys=' +environment.firebase.apiKey,requestBody)
+    }
+    public login(email:string, password:string)
+    {
+        const requestBody = {
+            "email" :email,
+            "password":password,
+            "returnSecureToken": true,
+        }
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signInEndPoint + '?' + 
+        'keys=' +environment.firebase.apiKey,requestBody)
     }
 }
